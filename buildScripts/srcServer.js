4d5359c7af0f5d6f2b1c9e68a79287e11e5ad531
TRENSER\1036 <madhuri.shaik@trenser.com>
2019-01-11 15:19:42 +0530

    Study

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,23 @@
 var express = require('express');
+var open  = require('open');
+var path = require('path')
 
 var app = express();
 
 var port = 8081;
-app.listen(port,"JSDEVENVDEMO",()=>
+app.listen(port,(err)=>
 {
+    if(err)
+    {
+        console.log(err)
+    }
+    else{
+        open('http://localhost:'+ port)
+    }
    //app.response.send("Helloooo")
+})
+
+app.get('/', (req, res) => 
+{
+    res.sendFile(path.join(__dirname, '../src/index.html'))
 })
